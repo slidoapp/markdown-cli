3514e598870c2cd827860de6b0863e0aba4ad9c4
Jozef Izso <izso@cisco.com>
2023-10-31 23:15:40 +0100

    Convert markdown file to HTML

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 import convert from './lib.mjs'
+import fs from 'fs'
 
-console.log('markdown')
-console.log(`  (runtime: node ${process.versions.node})`)
-console.log('')
+const args = process.argv.splice(2)
 
-convert('# markdown-cli\nMarkdown content.')
+if (args.length > 0) {
+    const markdownFile = args[0]
+    const content = fs.readFileSync(markdownFile, 'utf8')
+
+    convert(content)
+      .then((html) => {
+        process.stdout.write(String(html))
+        process.exit(0)
+      })
+      .catch((err) => {
+        process.stderr.write(err)
+        process.exit(1)
+      })
+} else {
+    console.log('markdown')
+    console.log(`  (runtime: node ${process.versions.node})`)
+}
--- src/lib.mjs
+++ src/lib.mjs
@@ -2,12 +2,9 @@ import { remark } from 'remark'
 import remarkHtml from 'remark-html'
 
 function convert(content) {
-  remark()
+  return remark()
     .use(remarkHtml)
     .process(content)
-    .then((file) => {
-      console.log(String(file))
-    })
 }
 
 export default convert
